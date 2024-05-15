b40c6549696e189423fd72d1b4fd660d73a9bc46
LET <bjs.6300.137@gmail.com>
2024-05-15 21:41:18 +0900

    firebase44

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:text_snd_prod/flavors.dart';
 import 'app.dart';
+import 'package:text_sns/gen/firebase_options_dev.dart' as dev;
+import 'package:text_sns/gen/firebase_options_prod.dart' as prod;
+import 'package:firebase_core/firebase_core.dart';
 
 FutureOr<void> main() async {
+  final flavor = F.appFlavor!;
+  final options = (flavor == Flavor.dev) ? 
+  await Firebase.initializeApp(options: );
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const App());
 }
